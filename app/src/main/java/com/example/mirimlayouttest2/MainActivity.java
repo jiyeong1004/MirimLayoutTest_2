015896dcbf89c38296f0c4d4cf89d0d44e5d1fbf
jiyeong1004 <ak1157@naver.com>
2020-10-12 20:05:02 +0900

    Solution 5-3

--- app/src/main/java/com/example/mirimlayouttest2/MainActivity.java
+++ app/src/main/java/com/example/mirimlayouttest2/MainActivity.java
@@ -2,10 +2,7 @@ package com.example.mirimlayouttest2;
 
 import android.graphics.Color;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 import android.widget.LinearLayout;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -21,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
         linear1.setBackgroundColor(Color.rgb(227,196,255));
         linear1.setOrientation(LinearLayout.VERTICAL);
 
-        Button btn = new Button(this);
+        /*Button btn = new Button(this);
         btn.setText("클릭해 보세요");
         btn.setBackgroundColor(Color.rgb(128, 65, 217));
         btn.setTextColor(Color.WHITE);
@@ -32,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Toast.makeText(getApplicationContext(), "Java 코드로 작성한 버튼입니다.", Toast.LENGTH_SHORT).show();
             }
-        });
+        });*/
 
         setContentView(linear1, params);
     }
