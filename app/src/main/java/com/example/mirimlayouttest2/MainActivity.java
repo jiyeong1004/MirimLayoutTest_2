430501b3cfb7f719de9000b23d8f56b9a7de3fd7
jiyeong1004 <ak1157@naver.com>
2020-10-12 20:41:26 +0900

    Commit and push

--- app/src/main/java/com/example/mirimlayouttest2/MainActivity.java
+++ app/src/main/java/com/example/mirimlayouttest2/MainActivity.java
@@ -1,12 +1,19 @@
 package com.example.mirimlayouttest2;
 
 import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    EditText edit1;
+    TextView text1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +37,28 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(), "Java 코드로 작성한 버튼입니다.", Toast.LENGTH_SHORT).show();
             }
         });*/
+
+        edit1 = new EditText(this);
+        text1 = new TextView(this);
+        /*LinearLayout.LayoutParams textParams = (LinearLayout.LayoutParams)text1.getLayoutParams();
+        textParams.topMargin = 20;
+        text1.setLayoutParams(textParams);*/
+        Button btn = new Button(this);
+        btn.setText("버튼입니다.");
+        edit1.setTextSize(20);
+        text1.setTextSize(20);
+        text1.setTextColor(Color.rgb(128, 65, 217));
+        text1.setTypeface(Typeface.MONOSPACE, Typeface.BOLD);
+        linear1.addView(edit1);
+        linear1.addView(btn);
+        linear1.addView(text1);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                text1.setText(edit1.getText().toString() + "이(가) 입력되었습니다.");
+            }
+        });
 
         setContentView(linear1, params);
     }
